c9322e3a5a98516b71ea896b79e74c6558789e53
Daria0104Isa <isakovadaria6@gmail.com>
2026-03-08 18:53:43 +0300

    Add redirects for /articles/* paths

---
+++ src/pages/articles/adjustment.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
+
+export default function ArticlesAdjustment() {
+  return <Redirect to="/knowledge/articles/adjustment" />;
+}
---
+++ src/pages/articles/basic-principles.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
+
+export default function ArticlesBasicPrinciples() {
+  return <Redirect to="/knowledge/articles/basic-principles" />;
+}
